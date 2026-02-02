a350961000b53cfa9fff86acdc8b1d7029002bf2
Sneha Dharel <snehadharel26@gmail.com>
2026-02-02 10:31:41 +0545

    Fixed admin pannel username and password error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#### SecureVault - Secure File Sharing Tool ####
-#### Developed by Sneha Dharel ####
+##### SecureVault - Secure File Sharing Tool #####
+##### Developed by Sneha Dharel #####
 
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox, scrolledtext
