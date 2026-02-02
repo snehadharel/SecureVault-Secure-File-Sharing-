3d20ce08b23e36b8a743b5993ec07e27fccfb631
Sneha Dharel <snehadharel26@gmail.com>
2026-02-02 10:28:32 +0545

    Implement a new register function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ### SecureVault - Secure File Sharing Tool ###
-### Developed by Sneha Dharel – Secure File Sharing Tool ###
+### Developed by Sneha Dharel ###
+
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox, scrolledtext
 import os
